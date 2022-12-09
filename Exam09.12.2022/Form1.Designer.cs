e73e34d355a0c15ed4693effff138a1f8d6618a3
П - 16 < - 16@DESKTOP-G9B4MHA>
2022-12-09 21:25:49 +0500

    Добавьте файлы проекта.

---
+++ Exam09.12.2022/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Exam09._12._2022
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelForTime = new System.Windows.Forms.Label();
+            this.timerTime = new System.Windows.Forms.Timer(this.components);
+            this.maskedTextBoxRemainder = new System.Windows.Forms.MaskedTextBox();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.richTextBoxRemainder = new System.Windows.Forms.RichTextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.timerRemainder = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelForTime
+            // 
+            this.labelForTime.AutoSize = true;
+            this.labelForTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelForTime.Location = new System.Drawing.Point(89, 60);
+            this.labelForTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelForTime.Name = "labelForTime";
+            this.labelForTime.Size = new System.Drawing.Size(115, 37);
+            this.labelForTime.TabIndex = 0;
+            this.labelForTime.Text = "Время";
+            // 
+            // timerTime
+            // 
+            this.timerTime.Interval = 1000;
+            this.timerTime.Tick += new System.EventHandler(this.timerTime_Tick);
+            // 
+            // maskedTextBoxRemainder
+            // 
+            this.maskedTextBoxRemainder.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.maskedTextBoxRemainder.Location = new System.Drawing.Point(74, 168);
+            this.maskedTextBoxRemainder.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.maskedTextBoxRemainder.Mask = "00:00:00";
+            this.maskedTextBoxRemainder.Name = "maskedTextBoxRemainder";
+            this.maskedTextBoxRemainder.Size = new System.Drawing.Size(149, 44);
+            this.maskedTextBoxRemainder.TabIndex = 2;
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelMessage.Location = new System.Drawing.Point(7, 226);
+            this.labelMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(273, 29);
+            this.labelMessage.TabIndex = 3;
+            this.labelMessage.Text = "Введите сообщение:";
+            // 
+            // richTextBoxRemainder
+            // 
+            this.richTextBoxRemainder.Location = new System.Drawing.Point(14, 269);
+            this.richTextBoxRemainder.Name = "richTextBoxRemainder";
+            this.richTextBoxRemainder.Size = new System.Drawing.Size(268, 96);
+            this.richTextBoxRemainder.TabIndex = 4;
+            this.richTextBoxRemainder.Text = "";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(12, 389);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(108, 43);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "Начать";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStop.Location = new System.Drawing.Point(174, 390);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(108, 42);
+            this.buttonStop.TabIndex = 6;
+            this.buttonStop.Text = "Стоп";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // timerRemainder
+            // 
+            this.timerRemainder.Interval = 500;
+            this.timerRemainder.Tick += new System.EventHandler(this.timerRemainder_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(24, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 37);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Текущее время:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(22, 107);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(257, 37);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Задайте время:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 465);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.richTextBoxRemainder);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.maskedTextBoxRemainder);
+            this.Controls.Add(this.labelForTime);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Remainder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelForTime;
+        private System.Windows.Forms.Timer timerTime;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxRemainder;
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.RichTextBox richTextBoxRemainder;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Timer timerRemainder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Exam09.12.2022/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exam09._12._2022
+{
+    public partial class Form1 : Form
+    {
+        private string currentTime;
+        private string remainderTime;
+        private string remainderText;
+        public Form1()
+        {
+            InitializeComponent();
+            timerTime.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timerTime_Tick(object sender, EventArgs e)
+        {
+            currentTime = DateTime.Now.ToString("HH:mm:ss");
+            labelForTime.Text = currentTime;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            remainderTime = maskedTextBoxRemainder.Text;
+            remainderText = richTextBoxRemainder.Text;
+            MessageBox.Show("Напоминание активировано");
+
+            timerRemainder.Start();
+        }
+
+        private void timerRemainder_Tick(object sender, EventArgs e)
+        {
+            if (currentTime == remainderTime)
+            {
+                timerRemainder.Stop();
+                MessageBox.Show(remainderText);
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Напоминание удалено!");
+            timerRemainder.Stop();
+        }
+    }
+}
